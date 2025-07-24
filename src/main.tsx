828c7257840ca079d7d0385e4b6dee8e00056007
TheLuQ <mistrz.luq@gmail.com>
2025-07-24 15:22:17 +0000

    Main page and docker backend added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App.tsx'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeProvider>,
 )
