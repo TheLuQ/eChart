b6f794798cd24c28e6658db25cc517b9577f640f
TheLuQ <mistrz.luq@gmail.com>
2025-07-29 11:53:59 +0000

    Futher improvements of grouping

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, FormControlLabel, FormLabel, List, ListItemButton, Radio, RadioGroup, TextField, Typography, type AutocompleteRenderInputParams } from '@mui/material'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import {
+  Autocomplete,
+  Button,
+  Divider,
+  FormControlLabel,
+  FormLabel,
+  List,
+  ListItemButton,
+  ListItemText,
+  Radio,
+  RadioGroup,
+  TextField,
+  type AutocompleteRenderInputParams
+} from '@mui/material'
 
 interface Instrument {
   name: string
@@ -10,12 +24,53 @@ interface Song {
   title: string
 }
 
+interface Sheet {
+  kind: string
+  id: string
+  name: string
+  mimeType: string
+  title: string,
+  instrument: string
+}
+
+interface SheetGroup {
+  title: string
+  sheets: Sheet[]
+}
+
+type GroupingKey = (sh: Sheet) => string
+
 function App() {
   const [instruments, setInstruments] = useState<Instrument[]>([])
   const [songs, setSongs] = useState<Song[]>([])
   const [selInstruments, selectInstruments] = useState<Instrument[]>([])
   const [selSongs, selectSongs] = useState<Song[]>([])
-  const [groupMethod, selectGroupMethod] = useState<string>('undefined')
+  const [groupMethod2, selectGroupMethod2] = useState<number>(-1)
+  const [sheets, setSheets] = useState<Sheet[]>([])
+
+  const confs = [
+    { state: 'instrument', fn: (sheet: Sheet) => sheet.instrument },
+    { state: 'type', fn: (sheet: Sheet) => sheet.mimeType }
+  ]
+  const currentConf = confs[groupMethod2]
+
+  function createGroups(sheets: Sheet[], getKey: GroupingKey) {
+    const resultMap = new Map<string, Sheet[]>()
+
+    sheets.forEach(sheet => {
+      const key = getKey(sheet)
+      const prev = resultMap.get(key) || []
+      prev.push(sheet)
+      resultMap.set(key, prev)
+    })
+
+    return Array.from(resultMap.entries())
+      .map<SheetGroup>(entry => ({
+        title: entry[0],
+        sheets: entry[1]
+      }))
+  }
+
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND}/instruments`)
@@ -25,66 +80,104 @@ function App() {
     fetch(`${import.meta.env.VITE_BACKEND}/titles`)
       .then(res => res.json())
       .then(data => setSongs(data))
+
+    fetch(`${import.meta.env.VITE_BACKEND}/sheets`)
+      .then(res => res.json())
+      .then(data => setSheets(data))
   }, [])
 
   return (
     <>
       <h1>eChart</h1>
-      <div>
+
+      <div hidden>
         <p>Select which sheets do you want to show</p>
-        <Autocomplete multiple value={selSongs} className='input'
+
+        <Autocomplete
+          multiple
+          value={selSongs}
+          className='input'
           disableCloseOnSelect={true}
           getOptionLabel={(opt) => opt.title}
           isOptionEqualToValue={(a, b) => a.title === b.title}
           onChange={(_, value) => selectSongs(value)}
-          renderInput={function (params: AutocompleteRenderInputParams): React.ReactNode {
-            return [<TextField sx={{ maxWidth: '500px' }} multiline  {...params} label="Select songs" variant="outlined" key={0} />]
-          }} options={songs}></Autocomplete>
+          renderInput={(params: AutocompleteRenderInputParams) => (
+            <TextField
+              sx={{ maxWidth: '500px' }}
+              multiline
+              {...params}
+              label="Select songs"
+              variant="outlined"
+            />
+          )}
+          options={songs}
+        />
 
-        <Autocomplete multiple value={selInstruments} className='input'
+        <Autocomplete
+          multiple
+          value={selInstruments}
+          className='input'
           disableCloseOnSelect={true}
           getOptionLabel={(opt) => opt.name}
           isOptionEqualToValue={(a, b) => a.name === b.name}
           onChange={(_, value) => selectInstruments(value)}
           autoHighlight={true}
-          renderInput={function (params: AutocompleteRenderInputParams): React.ReactNode {
-            return [<TextField sx={{ maxWidth: '500px' }} multiline  {...params} label="Select instruments" variant="outlined" key={1} />]
-          }}
-          options={instruments}>
-        </Autocomplete>
+          renderInput={(params: AutocompleteRenderInputParams) => (
+            <TextField
+              sx={{ maxWidth: '500px' }}
+              multiline
+              {...params}
+              label="Select instruments"
+              variant="outlined"
+            />
+          )}
+          options={instruments}
+        />
       </div>
 
-      <div className='radio' hidden>
-        <FormLabel id="demo-radio-buttons-group-label">Choose grouping type</FormLabel>
+      <div className='radio'>
+        <FormLabel id="demo-radio-buttons-group-label">
+          Choose grouping type
+        </FormLabel>
+
         <RadioGroup
           row
           aria-labelledby="demo-radio-buttons-group-label"
           name="radio-buttons-group"
-          value={groupMethod}
-          onChange={(_, value) => selectGroupMethod(value)}
+          value={groupMethod2}
         >
-          <FormControlLabel value="instrument" control={<Radio />} label="instrument" />
-          <FormControlLabel value="song" control={<Radio />} label="song" />
+          {confs.map((conf, index) => (
+            <FormControlLabel
+              key={conf.state}
+              checked={groupMethod2 === index}
+              onChange={() => selectGroupMethod2(index)}
+              value={conf.state}
+              control={<Radio />}
+              label={conf.state}
+            />
+          ))}
         </RadioGroup>
       </div>
 
-      <div hidden>
-        <Accordion className='acc' hidden={groupMethod === 'undefined'}>
-          <AccordionSummary
-            // expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
-          >
-            <Typography component="span">Dusty Trails</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <List sx={{ display: 'flex', flexDirection: 'column' }}>
-              <ListItemButton selected>Woodwind</ListItemButton>
-              <ListItemButton>Brass</ListItemButton>
-              <ListItemButton selected>Percussion</ListItemButton>
-            </List>
-          </AccordionDetails>
-        </Accordion>
+      <div hidden={groupMethod2 === -1}>
+        <List>
+          {groupMethod2 >= 0 &&
+            createGroups(sheets, currentConf.fn).map(group => (
+              <div key={group.title}>
+                <ListItemButton sx={{ justifyContent: 'space-between' }}>
+                  <ListItemText
+                    primary={group.title}
+                    secondary={`${group.sheets.length} files`}
+                  />
+                  <Button>
+                    <OpenInNewIcon color="primary" />
+                  </Button>
+                </ListItemButton>
+                <Divider />
+              </div>
+            ))
+          }
+        </List>
       </div>
     </>
   )
